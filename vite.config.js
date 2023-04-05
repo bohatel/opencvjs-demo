8f4da8b1057dd17a2600dbf716e6258b13d23756
bohatel <razvan.bohatel@gmail.com>
2023-04-05 11:52:21 +0300

    dependency and vite config updates

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    commonjs()
+  ],
   server: {
     port: 3001,
+    host: true
   },
   build: {
     target: 'esnext',
+    commonjsOptions: {
+      include: [/src/],
+    }
   },
 });
